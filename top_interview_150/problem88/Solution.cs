5797ca89308ad5da4b052a541320768803d0b05b
csuchorski <cezary.suchorski@gmail.com>
2023-06-20 20:40:14 +0200

    finished problem 88

--- top_interview_150/problem88/Solution.cs
+++ top_interview_150/problem88/Solution.cs
@@ -7,28 +7,34 @@ namespace Solution
             int writeIndex = m + n - 1;
             int guard1 = m - 1;
             int guard2 = n - 1;
-            while (guard2 >= 0 && guard1 >= 0)
+            while (guard2 >= 0)
             {
-
-                if (nums1[guard1] <= nums2[guard2])
+                if (guard1 >= 0)
+                {
+                    if (nums1[guard1] <= nums2[guard2])
+                    {
+                        nums1[writeIndex] = nums2[guard2];
+                        writeIndex--;
+                        guard2--;
+                        continue;
+                    }
+                    else
+                    {
+                        nums1[writeIndex] = nums1[guard1];
+                        writeIndex--;
+                        guard1--;
+                        continue;
+                    }
+                }
+                else
                 {
                     nums1[writeIndex] = nums2[guard2];
                     writeIndex--;
                     guard2--;
-                    continue;
                 }
-                else
-                {
-                    nums1[writeIndex] = nums1[guard1];
-                    writeIndex--;
-                    guard1--;
-                    continue;
-                }
+
             }
-            if (guard1 == -1)
-            {
-                nums1[writeIndex] = nums2[guard2];
-            }
+
 
             return nums1;
         }
