9e67bc73f91545da566eb7fe2f35f542d16d6b5d
csuchorski <cezary.suchorski@gmail.com>
2023-06-26 19:26:24 +0200

    problem169 passing but awfully slow solution

--- top_interview_150/problem169/Solution.cs
+++ top_interview_150/problem169/Solution.cs
@@ -5,7 +5,26 @@ namespace Solution
     {
         public static int MajorityElement(int[] nums)
         {
-            return 1;
+            int max = 0;
+            int majorityElement = nums[0];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int count = 0;
+
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[j] == nums[i])
+                    {
+                        count++;
+                    }
+                }
+                if (count > max)
+                {
+                    max = count;
+                    majorityElement = nums[i];
+                }
+            }
+            return majorityElement;
         }
     }
 }
